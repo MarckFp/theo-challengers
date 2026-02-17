981b671e9658ae7484a5cba1d70342bdc3e0206f
Marcos Fernández <maez@languagewire.com>
2026-02-17 11:07:22 +0100

    Added debug for ios and android init

---
+++ src-tauri/gen/android/app/src/main/java/com/marckfp/theo_challengers/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.marckfp.theo_challengers
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
